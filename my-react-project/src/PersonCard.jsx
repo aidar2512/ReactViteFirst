f4c21993bdc5dd09c3374ddffb5d4b21414c4918
Aidar Ormokoev <gumasroid@gmail.com>
2024-11-21 04:27:06 +0600

    Add PersonCard

---
+++ my-react-project/src/PersonCard.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const PersonCard = () => {
+    const person = {
+        name: "Айдар Айдар",
+        position: "Менеджер",
+        company: "Apple",
+        age: 20,
+        city: "Нью-Йорк",
+        experience: "5 лет"
+    };
+
+    return (
+        <div>
+            <h2>{person.name}</h2>
+            <p>Должность: {person.position}</p>
+            <p>Компания: {person.company}</p>
+            <p>Возраст: {person.age}</p>
+            <p>Город: {person.city}</p>
+            <p>Опыт: {person.experience}</p>
+        </div>
+    );
+};
+
+export default PersonCard;
